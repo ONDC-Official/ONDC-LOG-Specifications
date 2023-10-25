d5b4509289c744542042b5489440e5d09c5e6716
Tanya Madaan <tanyamadaan@Tanyas-MacBook-Air.local>
2023-10-25 12:47:55 +0530

    B2B Logistics API specs

---
+++ ui/tabs.js
@@ -0,0 +1,13 @@
+
+// tabs.js
+
+
+window.onload = function () {
+      let data = build_spec
+      initSchema(data["x-enum"])
+      initTag(data["x-tags"])
+      loadExample(data["x-examples"])
+      //addExample("on-demand")
+      loadFlows(data["x-flows"])
+      loadAttributes(data["x-attributes"])
+}
